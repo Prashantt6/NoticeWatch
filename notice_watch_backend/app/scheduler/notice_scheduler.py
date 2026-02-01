1a8619ab2d6560e5b89046be54f3cf755b0aa11c
Prashantt6 <royprashant14@gmail.com>
2026-02-01 15:29:34 +0545

    Handled server crash or gibrish response

--- notice_watch_backend/app/scheduler/notice_scheduler.py
+++ notice_watch_backend/app/scheduler/notice_scheduler.py
@@ -1,12 +1,27 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor
+from apscheduler.jobstores.memory import MemoryJobStore
 from app.services.scrapper  import getNotice
+
 
 scheduler = BackgroundScheduler()
 def start_scheduler():
     # print("Scheduler started")
-   
+    scheduler = BackgroundScheduler(
+        jobstores= {
+            "default": MemoryJobStore()
+        },
+        executors= {
+            "default": ThreadPoolExecutor(max_workers=1)
+        },
+        job_defaults = {
+            "colaesce": True,
+            "max_instance":1
+        },
+        timezone="UTC"
+    )
     scheduler.add_job(
-        getNotice,
+        func= getNotice,
         trigger= "interval",
         minutes = 10,
         id = "notice_scrapper",
@@ -14,3 +29,4 @@ def start_scheduler():
     )
 
     scheduler.start()
+    print("Notice scraper scheduler started")
--- notice_watch_backend/app/services/scrapper.py
+++ notice_watch_backend/app/services/scrapper.py
@@ -4,59 +4,73 @@ from app.db.database import SessionLocal
 from app.services.detection import checkChange
 from app.services.detection import store_hash_code
 
+URL= 'http://exam.ioe.edu.np/' #Notice page url
+BASE_URL = "http://exam.ioe.edu.np"   #Used to redirect to original page   
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (NoticeWatch Bot)"
+}
 
 def getNotice():
     print("hi from scrapper")
     db = SessionLocal()
     try:
-        res = requests.get('http://exam.ioe.edu.np/') # Fetching all the content of websites
-        soup = BeautifulSoup(res.content, 'html.parser') #Parsing  the content using html-parser
+        try:
+            res = requests.get(URL, headers= HEADERS,timeout=10) # Fetching all the content of websites
+            if(res.status_code!= 200):
+                print("Server error:", res.status_code)
+                return
+            soup = BeautifulSoup(res.content, 'html.parser') #Parsing  the content using html-parser
 
-        # Filtering the only notice
+            # Filtering only the notices
+            table = soup.find("table", id= "datatable") #notice table
 
-        table = soup.find("table", id= "datatable") #notice table
-
-        rows = table.tbody.find_all("tr") #getting rows of notice table
-        
-        for row in rows:
-            tds = row.find_all("td") #Extracting all the data from each rows
-
-            # print(tds)
-            sno = tds[0].get_text(strip = True) #serial no of notices
-            title_tag = tds[1].find("a")      #extracting title stored in a tag
-            title = title_tag.get_text(strip = True) #getting only title of notice
-            pdf_link = title_tag["href"]
-
-            notice_date = tds[2].get_text(strip = True)
-            view_link = tds[3].find("a")["href"] #getting the view link of notice   
-
-            BASE_URL = "http://exam.ioe.edu.np"   #Used to redirect to original page      
-            # print({
-            # "sno": sno,
-            # "title": title,
-            # "date": notice_date,
-            # "pdf": pdf_link,
-            # "view": view_link
-            #  })
-            notice ={
-                "sno": sno,
-                "title": title,
-                "date": notice_date,
-                "pdf": BASE_URL + pdf_link,
-                "view": BASE_URL + view_link
-            }
+            #handling bad structure
+            if not table: 
+                print("Invalid page: notice table not found")
+                return
+            rows = table.tbody.find_all("tr") #getting rows of notice table
             
-            
-            is_new =checkChange(db, notice)
-            
-            if is_new:
-                print("New Notice Detected")
-                store_hash_code(db)
+            for row in rows:
+                tds = row.find_all("td") #Extracting all the data from each rows
+
+                # print(tds)
+                sno = tds[0].get_text(strip = True) #serial no of notices
+                title_tag = tds[1].find("a")      #extracting title stored in a tag
+                title = title_tag.get_text(strip = True) #getting only title of notice
+                pdf_link = title_tag["href"]
+
+                notice_date = tds[2].get_text(strip = True)
+                view_link = tds[3].find("a")["href"] #getting the view link of notice   
+
+                   
+                # print({
+                # "sno": sno,
+                # "title": title,
+                # "date": notice_date,
+                # "pdf": pdf_link,
+                # "view": view_link
+                #  })
+                notice ={
+                    "sno": sno,
+                    "title": title,
+                    "date": notice_date,
+                    "pdf": BASE_URL + pdf_link,
+                    "view": BASE_URL + view_link
+                }
+                
+                #Detect change
+                is_new =checkChange(db, notice)
+                
+                if is_new:
+                    print("New Notice Detected")
+                    store_hash_code(db)
+        except Exception as e:
+            print("Scrapping failed", e)
     finally:
         db.close()
         
    
-getNotice()
+# getNotice()
 # notices =[]
 # notices.append(getNotice())
 
