bea6a4134990abe1444513704cca693ac937d451
CN-Liquid <adhikariaakash90@gmail.com>
2026-02-02 17:55:35 +0545

    Now the app polls the backend server every 5 minutes and updates the data

--- notice_watch_frontend/noticewatch/lib/main.dart
+++ notice_watch_frontend/noticewatch/lib/main.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:noticewatch/pages/notifications_list_page.dart';
 import 'package:noticewatch/pages/notice_page.dart';
 import 'package:noticewatch/repository.dart';
+import 'dart:async';
+
+// TODO : Add error handling
+// TODO : Add local notifications
+// TODO : Only update shared preferences ( notices ) is the server sends new data
 
 Map<String, WidgetBuilder> routes = {
   '/notice': (context) {
     return NoticePage();
   },
 };
+final service = NoticeService();
+
+void pollServer() async {
+  final data = await service.getData();
+  service.writeData(data);
+  Future.delayed(Duration(seconds: 5 * 60), pollServer);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final service = NoticeService();
+  pollServer();
 
-  final initialData = await service.getData();
-  service.writeData(initialData);
   runApp(MaterialApp(routes: routes, home: NotificationPage()));
 }
