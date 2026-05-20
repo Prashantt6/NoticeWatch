84a75fb3c02cd07581599d0e5dfdb7d62aa9d5d0
Prashantt6 <royprashant14@gmail.com>
2026-05-20 10:37:32 +0545

    feat/Device token storing end point created

---
+++ notice_watch_backend/app/api/devicetokens.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.db.models import DeviceToken
+
+router = APIRouter()
+
+
+class TokenRequest(BaseModel):
+    token: str
+
+
+class DeviceTokenService:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def storeToken(self, token: str):
+
+        existing = (
+            self.session.query(DeviceToken).filter(DeviceToken.token == token).first()
+        )
+        if existing:
+            return False
+
+        new_token = DeviceToken(token=token)
+
+        try:
+            self.session.add(new_token)
+            self.session.commit()
+
+            return True
+        except Exception:
+            self.session.rollback()
+
+            return False
+
+
+@router.post("/token")
+def storeDeviceToken(
+    data: TokenRequest,
+    db: Session = Depends(get_db),
+):
+    service = DeviceTokenService(db)
+
+    stored = service.storeToken(data.token)
+
+    if stored:
+        return {"message": "Device token stored"}
+
+    return {"message": "Token exists already"}
--- notice_watch_backend/app/main.py
+++ notice_watch_backend/app/main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.notices import router as notice_router
 from app.notifier.new_notice_notifier import router as notfier_router
+from app.api.devicetokens import router as device_token_router
 from app.scheduler.notice_scheduler import start_scheduler
 from app.firebase.firebase import initialize_firebase
-
+from app.db.database import Base, engine
+import app.db.models
 
 app = FastAPI()
 app.include_router(notice_router, prefix="/api/notices", tags=["Notices"])
 app.include_router(notfier_router, prefix="/api/notifier", tags=["Notifier"])
+app.include_router(device_token_router, prefix="/api/device", tags=["Device Token"])
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,6 +25,7 @@ app.add_middleware(
 @app.on_event("startup")
 def startup_event():
 
+    Base.metadata.create_all(bind=engine)
     initialize_firebase()
     start_scheduler()
 
