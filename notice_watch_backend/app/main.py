381807f9853d408f2b1b1b168e5e1c1429dbd25d
Prashantt6 <royprashant14@gmail.com>
2026-05-26 17:59:14 +0545

    fix/Missing files from main

--- notice_watch_backend/app/main.py
+++ notice_watch_backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.notices import router as notice_router
-from app.notifier.new_notice_notifier import router as notfier_router
 from app.api.devicetokens import router as device_token_router
 from app.scheduler.notice_scheduler import start_scheduler
 from app.firebase.firebase import initialize_firebase
@@ -9,7 +8,6 @@ from app.db.database import Base, engine
 
 app = FastAPI()
 app.include_router(notice_router, prefix="/api/notices", tags=["Notices"])
-app.include_router(notfier_router, prefix="/api/notifier", tags=["Notifier"])
 app.include_router(device_token_router, prefix="/api/device", tags=["Device Token"])
 
 app.add_middleware(
